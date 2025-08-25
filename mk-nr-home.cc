43a5b89a3b8bd378a825c6bfec19ff3802fea97b
itsmesaim <saimkaskar69@gmail.com>
2025-08-25 09:09:29 +0100

    version 3

--- mk-nr-home.cc
+++ mk-nr-home.cc
@@ -1,202 +1,148 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/csma-module.h"
+#include "ns3/mobility-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/flow-monitor-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
-#include "ns3/netanim-module.h"
-#include "ns3/mobility-module.h"
 
-// 5G-LENA (NR) v4.x
-#include "ns3/nr-module.h"
-#include "ns3/antenna-module.h"
-#include "ns3/nr-point-to-point-epc-helper.h"
+#include "ns3/nr-module.h"                         // NrHelper, CcBwpCreator, etc.
+#include "ns3/nr-point-to-point-epc-helper.h"      // EPC for NR
 
 using namespace ns3;
 
-static void SetPos(Ptr<Node> n, double x, double y) {
-  MobilityHelper mob; mob.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  NodeContainer one(n); mob.Install(one);
-  n->GetObject<MobilityModel>()->SetPosition(Vector(x,y,0));
-}
+int main (int argc, char *argv[])
+{
+  // ---- Simulation params (override via --gNbNum= --ueNum= --simTime= etc.) ----
+  uint16_t gNbNum = 1;
+  uint16_t ueNum = 2;
+  double centralFreq = 28e9;       // 28 GHz
+  double bandwidth  = 100e6;       // 100 MHz
+  uint8_t numCc     = 1;           // one CC → one BWP
+  uint32_t numerology = 1;         // μ=1 ≈ 30 kHz SCS
+  double simTimeSec = 3.0;         // total sim time
+  bool logging = false;
 
-int main (int argc, char* argv[]) {
-  Time::SetResolution(Time::NS);
-  LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+  CommandLine cmd;
+  cmd.AddValue ("gNbNum", "Number of gNBs", gNbNum);
+  cmd.AddValue ("ueNum", "Number of UEs", ueNum);
+  cmd.AddValue ("centralFreq", "Central frequency (Hz)", centralFreq);
+  cmd.AddValue ("bandwidth", "Channel bandwidth (Hz)", bandwidth);
+  cmd.AddValue ("numCc", "Number of component carriers per band", numCc);
+  cmd.AddValue ("numerology", "NR numerology (0..4)", numerology);
+  cmd.AddValue ("simTime", "Simulation time (s)", simTimeSec);
+  cmd.AddValue ("logging", "Enable some logs", logging);
+  cmd.Parse (argc, argv);
 
-  // ----------- City layout -----------
-  Ptr<Node> core = CreateObject<Node>();         // central core
-  NodeContainer homeHosts; homeHosts.Create(2);
-  Ptr<Node> homeGw = CreateObject<Node>();       // will be UE
-  NodeContainer officeHosts; officeHosts.Create(2);
-  Ptr<Node> officeGw = CreateObject<Node>();
-  NodeContainer uniHosts; uniHosts.Create(2);
-  Ptr<Node> uniGw = CreateObject<Node>();
-  NodeContainer iotHosts; iotHosts.Create(2);
-  Ptr<Node> iotGw = CreateObject<Node>();
+  if (logging)
+  {
+    LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
+    LogComponentEnable ("UdpServer", LOG_LEVEL_INFO);
+  }
 
-  InternetStackHelper stack;
-  stack.Install(core);
-  stack.Install(homeGw);   stack.Install(homeHosts);
-  stack.Install(officeGw); stack.Install(officeHosts);
-  stack.Install(uniGw);    stack.Install(uniHosts);
-  stack.Install(iotGw);    stack.Install(iotHosts);
+  // ---- Create nodes ----
+  NodeContainer gNbNodes;  gNbNodes.Create (gNbNum);
+  NodeContainer ueNodes;   ueNodes.Create (ueNum);
 
-  // LAN helpers
-  CsmaHelper csma; csma.SetChannelAttribute("DataRate", DataRateValue(DataRate("100Mbps")));
-  csma.SetChannelAttribute("Delay", TimeValue(MilliSeconds(0.5)));
-  PointToPointHelper p2p; p2p.SetDeviceAttribute("DataRate", StringValue("50Mbps"));
-  p2p.SetChannelAttribute("Delay", StringValue("2ms"));
+  // Remote host (the "Internet" beyond the PGW)
+  NodeContainer remoteHostContainer; 
+  remoteHostContainer.Create (1);
+  Ptr<Node> remoteHost = remoteHostContainer.Get (0);
 
-  // ---- Build LANs (CSMA) ----
-  NodeContainer homeLan;   homeLan.Add(homeGw);     homeLan.Add(homeHosts);
-  NodeContainer officeLan; officeLan.Add(officeGw); officeLan.Add(officeHosts);
-  NodeContainer uniLan;    uniLan.Add(uniGw);       uniLan.Add(uniHosts);
-  NodeContainer iotLan;    iotLan.Add(iotGw);       iotLan.Add(iotHosts);
+  // ---- EPC / Core ----
+  Ptr<NrPointToPointEpcHelper> epcHelper = CreateObject<NrPointToPointEpcHelper> ();
+  Ptr<Node> pgw = epcHelper->GetPgwNode ();
 
-  NetDeviceContainer homeLanDevs   = csma.Install(homeLan);
-  NetDeviceContainer officeLanDevs = csma.Install(officeLan);
-  NetDeviceContainer uniLanDevs    = csma.Install(uniLan);
-  NetDeviceContainer iotLanDevs    = csma.Install(iotLan);
+  // Internet between PGW <-> RemoteHost
+  PointToPointHelper p2p;
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("100Gbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("1ms"));
+  NetDeviceContainer internetDevs = p2p.Install (pgw, remoteHost);
 
-  // ---- NR EPC stack (HomeGW <-> gNB over the air) ----
-  Ptr<NrHelper> nr = CreateObject<NrHelper>();
-  Ptr<NrPointToPointEpcHelper> epc = CreateObject<NrPointToPointEpcHelper>();
-  nr->SetEpcHelper(epc);
+  InternetStackHelper internet;
+  internet.Install (remoteHostContainer);         // IP stack on remote host
+  internet.Install (ueNodes);                     // IP stack on UEs (EPC assigns UE IPs)
 
-  Ptr<Node> pgw = epc->GetPgwNode();
+  Ipv4AddressHelper ipv4h;
+  ipv4h.SetBase ("1.0.0.0", "255.0.0.0");        // simple /8 for PGW<->RemoteHost link
+  Ipv4InterfaceContainer ifaces = ipv4h.Assign (internetDevs);
+  Ipv4Address pgwAddr       = ifaces.GetAddress (0);  // on PGW
+  Ipv4Address remoteHostIp  = ifaces.GetAddress (1);  // on RemoteHost
 
-  // Create one gNB (separate node from CORE for clarity) and 1 UE (Home-GW)
-  NodeContainer gNbNodes; gNbNodes.Create(1);
-  NodeContainer ueNodes;  ueNodes.Add(homeGw);
+  // Route on RemoteHost to UE subnet (EPC gives UEs 7.0.0.0/8 by default)
+  Ipv4StaticRoutingHelper rtHelper;
+  Ptr<Ipv4StaticRouting> rhRoute = rtHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
+  rhRoute->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), pgwAddr, 1);
 
-  // Positions (NetAnim)
-  SetPos(core, 50, 40);
-  SetPos(gNbNodes.Get(0), 40, 40);
-  SetPos(homeGw, 10, 60);
-  for (uint32_t i=0;i<homeHosts.GetN();++i)  SetPos(homeHosts.Get(i), 5 + 8*i, 70);
-  SetPos(officeGw, 90, 60);
-  for (uint32_t i=0;i<officeHosts.GetN();++i) SetPos(officeHosts.Get(i), 85 + 8*i, 70);
-  SetPos(uniGw,    90, 20);
-  for (uint32_t i=0;i<uniHosts.GetN();++i)    SetPos(uniHosts.Get(i), 85 + 8*i, 10);
-  SetPos(iotGw,    10, 20);
-  for (uint32_t i=0;i<iotHosts.GetN();++i)    SetPos(iotHosts.Get(i), 5 + 8*i, 10);
+  // ---- NR Helper and spectrum/BWPs (new v4.x API) ----
+  Ptr<NrHelper> nr = CreateObject<NrHelper> ();
+  nr->SetEpcHelper (epcHelper);
 
-  // Connect PGW <-> CORE via P2P (so the rest routes via CORE)
-  NetDeviceContainer pgwCore = p2p.Install(NodeContainer(pgw, core));
+  std::vector<CcBwpCreator::SimpleOperationBandConf> bands;
+  bands.emplace_back (centralFreq, bandwidth, numCc);
 
-  // ---- NR Bandwidth Parts (v4.x API) ----
-  double freqGHz = 3.5;            // FR1-ish
-  double bwMHz   = 20.0;           // 20 MHz carrier
-  CcBwpCreator ccBwpCreator;
-  CcBwpCreator::SimpleOperationBandConf bandConf(freqGHz, bwMHz, 1,
-      BandwidthPartInfo::SubcarrierSpacing::kHz30);
-  OperationBandInfo band = ccBwpCreator.CreateOperationBand(bandConf);
-  BandwidthPartInfoPtrVector allBwps = CcBwpCreator::GetAllBwps(band);
+  // Create BWPs & channels (no InitializeOperationBand in v4.x)
+  auto [totalBw, allBwps] = nr->CreateBandwidthParts (bands);
 
-  nr->InitializeOperationBand(&band);
+  // ---- gNB / UE devices ----
+  NetDeviceContainer gnbDevs = nr->InstallGnbDevice (gNbNodes, allBwps);
+  NetDeviceContainer ueDevs  = nr->InstallUeDevice  (ueNodes,  allBwps);
 
-  // Install NR devices
-  NetDeviceContainer gNbDevs = nr->InstallGnbDevice(gNbNodes, allBwps);
-  NetDeviceContainer ueDevs  = nr->InstallUeDevice(ueNodes,  allBwps);
+  // Set numerology μ on BWP 0 for all devices (your old 30 kHz intent)
+  for (uint32_t i = 0; i < gnbDevs.GetN (); ++i)
+  {
+    Ptr<NrGnbPhy> gnbPhy0 = NrHelper::GetGnbPhy (gnbDevs.Get (i), 0);
+    gnbPhy0->SetAttribute ("Numerology", UintegerValue (numerology));
+  }
+  for (uint32_t i = 0; i < ueDevs.GetN (); ++i)
+  {
+    Ptr<NrUePhy> uePhy0 = NrHelper::GetUePhy (ueDevs.Get (i), 0);
+    uePhy0->SetAttribute ("Numerology", UintegerValue (numerology));
+  }
 
-  // Attach UE to closest gNB
-  nr->AttachToClosestGnb(ueDevs, gNbDevs);
+  // ---- Simple placement ----
+  // gNB at origin; UEs on a line at y=0, spaced by 10 m
+  MobilityHelper mob;
+  Ptr<ListPositionAllocator> gnbPos = CreateObject<ListPositionAllocator> ();
+  gnbPos->Add (Vector (0.0, 0.0, 10.0));              // 10 m high
+  mob.SetPositionAllocator (gnbPos);
+  mob.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mob.Install (gNbNodes);
 
-  // EPC assigns UE IPs (7.0.0.0/8 by default)
-  epc->AssignUeIpv4Address(ueDevs);
+  Ptr<ListPositionAllocator> uePos = CreateObject<ListPositionAllocator> ();
+  for (uint32_t i = 0; i < ueNum; ++i)
+  {
+    uePos->Add (Vector (5.0 + 10.0 * i, 0.0, 1.5));
+  }
+  mob.SetPositionAllocator (uePos);
+  mob.Install (ueNodes);
 
-  // ---- IP addressing for the LANs & PGW-CORE ----
-  Ipv4AddressHelper addr;
-  addr.SetBase("10.1.1.0", "255.255.255.0"); Ipv4InterfaceContainer ifHomeLan   = addr.Assign(homeLanDevs);
-  addr.SetBase("10.1.2.0", "255.255.255.0"); Ipv4InterfaceContainer ifOfficeLan = addr.Assign(officeLanDevs);
-  addr.SetBase("10.1.3.0", "255.255.255.0"); Ipv4InterfaceContainer ifUniLan    = addr.Assign(uniLanDevs);
-  addr.SetBase("10.1.4.0", "255.255.255.0"); Ipv4InterfaceContainer ifIotLan    = addr.Assign(iotLanDevs);
+  // ---- Attach UEs and set bearer ----
+  nr->AttachToClosestGnb (ueDevs, gnbDevs);  // initial association
+  NrEpsBearer bearer (NrEpsBearer::NGBR_LOW_LAT_EMBB);
+  nr->ActivateDataRadioBearer (ueDevs, bearer);
 
-  addr.SetBase("10.255.100.0", "255.255.255.252");
-  Ipv4InterfaceContainer ifPgwCore = addr.Assign(pgwCore); // index 0 = PGW, 1 = CORE
+  // ---- UL traffic: UEs -> RemoteHost (UDP) ----
+  uint16_t ulPort = 4444;
+  UdpServerHelper ulServer (ulPort);
+  ApplicationContainer serverApps = ulServer.Install (remoteHostContainer);
+  serverApps.Start (Seconds (0.2));
+  serverApps.Stop  (Seconds (simTimeSec));
 
-  // Route the UE-net (7.0.0.0/8) via PGW from CORE
-  Ipv4StaticRoutingHelper srh;
-  Ptr<Ipv4StaticRouting> coreStatic = srh.GetStaticRouting(core->GetObject<Ipv4>());
-  coreStatic->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"),
-                                ifPgwCore.GetAddress(0), 1);
+  for (uint32_t i = 0; i < ueDevs.GetN (); ++i)
+  {
+    UdpClientHelper ulClient (remoteHostIp, ulPort);
+    ulClient.SetAttribute ("MaxPackets", UintegerValue (0));      // unlimited
+    ulClient.SetAttribute ("Interval",   TimeValue (MilliSeconds (2)));
+    ulClient.SetAttribute ("PacketSize", UintegerValue (512));
+    ApplicationContainer c = ulClient.Install (ueNodes.Get (i));
+    c.Start (Seconds (0.4 + 0.05 * i));
+    c.Stop  (Seconds (simTimeSec));
+  }
 
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+  // ---- Run ----
+  Simulator::Stop (Seconds (simTimeSec));
+  Simulator::Run ();
+  Simulator::Destroy ();
 
-  // ---- Apps (same as your wired city) ----
-  // 1) Home H1 -> University U1 (UDP Echo)
-  Ptr<Node> H1 = homeHosts.Get(0);
-  Ptr<Node> U1 = uniHosts.Get(0);
-  uint16_t echoPort = 9;
-  UdpEchoServerHelper echoServer(echoPort);
-  auto echoSrvApp = echoServer.Install(U1);
-  echoSrvApp.Start(Seconds(1.0)); echoSrvApp.Stop(Seconds(12.0));
-  Ipv4Address u1Addr = U1->GetObject<Ipv4>()->GetAddress(1,0).GetLocal();
-  UdpEchoClientHelper echoClient(u1Addr, echoPort);
-  echoClient.SetAttribute("MaxPackets", UintegerValue(6));
-  echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-  echoClient.SetAttribute("PacketSize", UintegerValue(512));
-  auto echoCliApp = echoClient.Install(H1);
-  echoCliApp.Start(Seconds(2.0)); echoCliApp.Stop(Seconds(12.0));
-
-  // 2) IoT I1 -> Home H2 (UDP OnOff to a sink)
-  Ptr<Node> I1 = iotHosts.Get(0);
-  Ptr<Node> H2 = homeHosts.Get(1);
-  uint16_t iotPort = 4000;
-  Address sinkAddr (InetSocketAddress (H2->GetObject<Ipv4>()->GetAddress(1,0).GetLocal(), iotPort));
-  PacketSinkHelper sinkUdp ("ns3::UdpSocketFactory", sinkAddr);
-  auto sinkApp = sinkUdp.Install(H2);
-  sinkApp.Start(Seconds(1.0)); sinkApp.Stop(Seconds(12.0));
-  OnOffHelper onoff ("ns3::UdpSocketFactory", sinkAddr);
-  onoff.SetAttribute("DataRate", StringValue("2Mbps"));
-  onoff.SetAttribute("PacketSize", UintegerValue(300));
-  onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
-  onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
-  auto iotApp = onoff.Install(I1);
-  iotApp.Start(Seconds(3.0)); iotApp.Stop(Seconds(12.0));
-
-  // 3) Office O1 -> University U2 (TCP BulkSend)
-  Ptr<Node> O1 = officeHosts.Get(0);
-  Ptr<Node> U2 = uniHosts.Get(1);
-  uint16_t tcpPort = 5001;
-  Address sinkAddrTcp (InetSocketAddress (U2->GetObject<Ipv4>()->GetAddress(1,0).GetLocal(), tcpPort));
-  PacketSinkHelper sinkTcp ("ns3::TcpSocketFactory", sinkAddrTcp);
-  auto sinkTcpApp = sinkTcp.Install(U2);
-  sinkTcpApp.Start(Seconds(1.0)); sinkTcpApp.Stop(Seconds(12.0));
-  BulkSendHelper bulk ("ns3::TcpSocketFactory", sinkAddrTcp);
-  bulk.SetAttribute("MaxBytes", UintegerValue(0));
-  bulk.SetAttribute("SendSize", UintegerValue(1024));
-  auto bulkApp = bulk.Install(O1);
-  bulkApp.Start(Seconds(4.0)); bulkApp.Stop(Seconds(12.0));
-
-  // ---- Tracing ----
-  csma.EnablePcap("mkNR-home",   homeLanDevs.Get(1), true);
-  csma.EnablePcap("mkNR-office", officeLanDevs.Get(1), true);
-  csma.EnablePcap("mkNR-uni",    uniLanDevs.Get(1), true);
-  csma.EnablePcap("mkNR-iot",    iotLanDevs.Get(1), true);
-  p2p.EnablePcapAll("mkNR-pgw-core");
-
-  FlowMonitorHelper fmh; Ptr<FlowMonitor> fm = fmh.InstallAll();
-
-  AnimationInterface anim("mk-nr-home.xml");
-  anim.UpdateNodeDescription(core, "CORE");
-  anim.UpdateNodeDescription(homeGw, "Home-UE");
-  anim.UpdateNodeDescription(gNbNodes.Get(0), "gNB");
-  anim.UpdateNodeDescription(officeGw, "Office-GW");
-  anim.UpdateNodeDescription(uniGw, "Uni-GW");
-  anim.UpdateNodeDescription(iotGw, "IoT-GW");
-  for (uint32_t i=0;i<homeHosts.GetN();++i)  anim.UpdateNodeDescription(homeHosts.Get(i),  "H"+std::to_string(i+1));
-  for (uint32_t i=0;i<officeHosts.GetN();++i) anim.UpdateNodeDescription(officeHosts.Get(i),"O"+std::to_string(i+1));
-  for (uint32_t i=0;i<uniHosts.GetN();++i)    anim.UpdateNodeDescription(uniHosts.Get(i),  "U"+std::to_string(i+1));
-  for (uint32_t i=0;i<iotHosts.GetN();++i)    anim.UpdateNodeDescription(iotHosts.Get(i),  "I"+std::to_string(i+1));
-
-  Simulator::Stop(Seconds(12.0));
-  Simulator::Run();
-  fm->SerializeToXmlFile("mkNR-flow.xml", true, true);
-  Simulator::Destroy();
   return 0;
 }
